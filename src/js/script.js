aa9416a8e051bb8f79e0f5d4c8bdb7a812bf9e19
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-01-09 10:27:54 +0500

    IMPROVE performance

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,5 @@
-import { getStorage,ref,listAll, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBmVFcgSM9FtDXYId4XcQoJKep3K7uDxCg",
-    authDomain: "beauty-268c5.firebaseapp.com",
-    projectId: "beauty-268c5",
-    storageBucket: "beauty-268c5.appspot.com",
-    messagingSenderId: "307850780217",
-    appId: "1:307850780217:web:68fa00e2bb87c0c2ebb7e8"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app)
-const storageRef = ref(storage, '/images/slider');
-
-function loadImages() {
-    listAll(storageRef)
-        .then(function (result) {
-            result.items.forEach(function (item) {
-                getDownloadURL(item)
-                    .then(function (url) {
-                        // Create img element
-                        let imgElement = document.createElement('img');
-                        imgElement.alt = 'girl';
-                        imgElement.src = url;
-
-                        // Append the img element to the slider
-                        document.querySelector('.owl-carousel').appendChild(imgElement);
-                    })
-                    .catch(function (error) {
-                        console.error('Error getting download URL:', error);
-                    });
-            });
-        })
-        .catch(function (error) {
-            console.error('Error getting images from Firebase Storage:', error);
-        });
-}
-function initializeOwlCarousel() {
+import {loadImages} from "./config.js";
+export function initializeOwlCarousel() {
     $('.owl-carousel').owlCarousel({
         items: 5,
         loop: true,
@@ -69,8 +28,5 @@ function initializeOwlCarousel() {
         }
     });
 }
-document.addEventListener("DOMContentLoaded", loadImages);
-window.onload = function (){
-    initializeOwlCarousel();
-}
+
 
